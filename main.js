241d105a4e5c062a0896b914006fa341413ce156
Admin <1119321.ab@mail.ru>
2024-11-13 21:07:12 +0100

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById("toggleDescription").addEventListener("click", function () {
+    const description = document.getElementById("description");
+    if (description.style.display === "none") {
+        description.style.display = "block";
+        this.textContent = "Скрыть описание";
+    } else {
+        description.style.display = "none";
+        this.textContent = "Показать описание";
+    }
+});
